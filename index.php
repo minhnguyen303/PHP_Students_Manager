b6c19f0355a13b29d8ba544ceb182d18e69ec5e4
minhnguyen303 <minh09az09@gmail.com>
2021-01-04 11:42:30 +0700

    Add content of index file

--- index.php
+++ index.php
@@ -1,1 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Students Manager</title>
+</head>
+<body>
+<form action="" method="post" id="formSearch" hidden="hidden"></form>
+<table>
+    <caption><h1>Danh sách học sinh</h1></caption>
+    <tr>
+        <td colspan="8" id="colSearch">
+            <label for="inputSearch">Search: </label>
+            <input id="inputSearch" name="inputSearch" form="formSearch" placeholder="Tên học sinh cần tìm kiếm">
+            <button type="submit" form="formSearch">Search</button>
+        </td>
+    </tr>
+    <tr>
+        <th>STT</th>
+        <th>Tên</th>
+        <th>Tuổi</th>
+        <th>Giới tính</th>
+        <th>Năm sinh</th>
+        <th>Mã sinh viên</th>
+        <th>Ngành học</th>
+        <th>Quê quán</th>
+        <th></th>
+    </tr>
+</table>
+</body>
+</html>
 <?php
