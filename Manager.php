eb9469ae87bc982ecdffde84374df2b8b29f9eb9
minhnguyen303 <minh09az09@gmail.com>
2021-01-04 15:01:04 +0700

    Add function update

--- Manager.php
+++ Manager.php
@@ -9,4 +9,44 @@ class Manager
     {
         self::$students[] = $student;
     }
+
+    public function update($index, $student)
+    {
+        self::$students[$index] = $student;
+    }
+
+    public function get($index)
+    {
+        
+    }
+
+    public function delete($index)
+    {
+        
+    }
+
+    public function search($index)
+    {
+        
+    }
+
+    public function sortAZ()
+    {
+        
+    }
+
+    public function sortZA()
+    {
+        
+    }
+
+    public function save()
+    {
+        
+    }
+
+    public function load()
+    {
+        
+    }
 }
